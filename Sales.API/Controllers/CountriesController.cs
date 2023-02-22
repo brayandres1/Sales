08aa0ca3e37146f286b2b251cc0c8e0db002524b
BRAYAN ANDRES VELEZ LOPEZ <brayanvelezlopez@hotmail.com>
2023-02-22 15:25:05 -0500

    Complete countries API Controller

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -7,7 +7,7 @@ namespace Sales.API.Controllers
 {
     [ApiController]
     [Route("/api/countries")]
-    public class CountriesController : ControllerBase  
+    public class CountriesController : ControllerBase
     {
         private readonly DataContext _context;
         public CountriesController(DataContext contex)
@@ -16,11 +16,20 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet]
-
         public async Task<IActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
+        }
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country); 
         }
 
         [HttpPost]
@@ -31,5 +40,25 @@ namespace Sales.API.Controllers
             return Ok(country);
         }
 
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
