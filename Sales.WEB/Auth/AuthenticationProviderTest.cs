a9312cbc041caf6a2caec5dfd2bfd3a24e8e2224
BRAYAN ANDRES VELEZ LOPEZ <brayanvelezlopez@hotmail.com>
2023-03-13 17:29:46 -0500

    Security API, DB, WEB

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var AndresVelez = new ClaimsIdentity(new List<Claim>
+             {
+                 new Claim("FirstName", "Andres"),
+                 new Claim("LastName", "Velez"),
+                 new Claim(ClaimTypes.Name, "avelez@yopmail.com"),
+                 new Claim(ClaimTypes.Role, "Admin")
+             },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(AndresVelez)));
+
+        }
+    }
+}
