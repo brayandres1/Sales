f537035a17ead162c44a211ddbf400bc844849bc
BRAYAN ANDRES VELEZ LOPEZ <brayanvelezlopez@hotmail.com>
2023-03-27 13:35:39 -0500

    Edit User Finish

--- Sales.Shared/DTOs/EmailDTO.cs
+++ Sales.Shared/DTOs/EmailDTO.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Sales.Shared.DTOs
 {
-    internal class EmailDTO
+    public class EmailDTO
     {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Debes ingresar un correo válido.")]
+        public string Email { get; set; } = null!;
     }
 }
--- Sales.WEB/Repositories/IRepository.cs
+++ Sales.WEB/Repositories/IRepository.cs
@@ -12,7 +12,7 @@
 
         Task<HttpResponseWrapper<object>> Delete(string url);
 
-        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);      
 
         Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
     }
